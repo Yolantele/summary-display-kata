c56c7279e84bc544d8039ea8370e220573b76009
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-12-23 15:17:51 +0000

    Sets colour pallette constants for the app

---
+++ src/assets/styles.js
@@ -0,0 +1,20 @@
+
+const FONT = 'Roboto';
+
+const COLOURS = {
+  key: "#ebeef4",
+  primary: "#5496bf",
+  accent: "#ea5468",
+  lightGrey: "#F8F8F8",
+  darkGrey: "#7F7F7F",
+  black: "#000000",
+  offWhite: "#f4fff9",
+  white: "#ffffff",
+
+  transBlack: "rgb(127, 127, 127, 0.25)"
+};
+
+module.exports = {
+  font: FONT,
+  colours: COLOURS
+};
