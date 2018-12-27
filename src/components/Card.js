9028f5354418c5540703e3051ab6da7612c8afef
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-12-27 19:12:29 +0000

    Tweaks card display logics, cotntroling text displays

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,22 +15,23 @@ class Card extends React.Component {
     const cardStyle = [localStyles.container];
 
     let lengthOfDescription = CHAR_LENGTH;
-    let lengthOfTitle = CHAR_LENGTH;
+    let lengthOfTitle = CHAR_LENGTH / 4 ;
 
     if (height) cardStyle.push({height: height});
+
     if (width) {
       cardStyle.push({width: width}) 
       lengthOfDescription = width /CARD_SIZE_TO_TEXT_PROPORTION;
       lengthOfTitle = width /CARD_SIZE_TO_TITLE_PROPORTION;
     }
-
     
     let title = `${card.name} - ${card.address1}`;
-    let description = `${card.listing_text.substring(0, lengthOfDescription)}...`;
+    let description = `${card.listing_text}`;
     let details = `${card.city}, ${card.postcode}`;
 
-    if (title.length > lengthOfTitle) {
-      title = title.substring(0, lengthOfTitle) + '...';
+    if (title.length > lengthOfTitle) title = title.substring(0, lengthOfTitle) + '...';
+    if (description.length > lengthOfDescription) {
+      description = description.substring(0, lengthOfDescription) + '...'
     }
   
     return(
@@ -39,11 +40,11 @@ class Card extends React.Component {
         alt={card.id} 
         key={card.id}
         onClick={() => {alert('mouse is left')}} 
-        onMouseOver={() => {alert('mouse is left')}}
+        onMouseOver={() => {}}
       >
-        <h2 style={localStyles.header}>{title}</h2>
-        <h4 style={localStyles.texts}>{description}</h4>
-        <h4 style={localStyles.texts}>{details}</h4>
+        <h3 style={localStyles.header}>{title}</h3>
+        <h5 style={localStyles.texts}>{description}</h5>
+        <h5 style={localStyles.texts}>{details}</h5>
       </div>
     )
     
@@ -52,16 +53,16 @@ class Card extends React.Component {
 
 const localStyles = {
   container: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-around",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
     cursor: 'pointer',
     backgroundColor: Styles.colours.white,
     padding: 40,
-    margin: 40, 
+    margin: 5, 
     borderRadius: 15,
     boxShadow: '2px 2px 50px lightgray',
-    width: 300
+    width: 230,
   },
   header:{
     fontFamily: Styles.font,
@@ -78,7 +79,7 @@ const localStyles = {
     margin: 0, 
     paddingTop: 5,
     paddingBottom: 5,
-    opacity: 0.6
+    opacity: 0.6,
   }
 };
 
